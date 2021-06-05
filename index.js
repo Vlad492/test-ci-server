3de2f93c52441e0728bd5bb3322a5f1ba43775db
vlad.hleb <vladyslav.hleb@gen.tech>
2021-06-05 18:51:18 +0300

    added routes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 app.listen(80,()=>console.log("Server has been started"))
 
 app.get('/',(req,res)=>{
-    res.send(new Date())
+    res.send(new Date().toLocaleDateString())
 })
 app.get('/hello',(req,res)=>{
     res.send('hello-world')
